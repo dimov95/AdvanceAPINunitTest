87a6e2353a8f3bf8b06b71b03a7b27674e3bb064
dimov95 <95aleksandyr@gmail.com>
2024-12-23 22:03:01 +0200

    first git push

---
+++ ApiTestingWorkshop/GlobalConstants.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTestingWorkshop
+{
+    internal class GlobalConstants
+    {
+        public const string BaseUrl = "http://localhost:5000/api";
+
+        public static string AuthenticateUser(string email, string password)
+        {
+            var resource = email == "admin@gmail.com" ? "user/admin-login" : "user/login";
+
+            var authClient = new RestClient(BaseUrl);
+            var restRequest = new RestRequest(resource, Method.Post);
+            restRequest.AddJsonBody(new{email, password });
+            var response = authClient.Post(restRequest);
+            return "";
+        }
+
+    }
+}
---
+++ ApiTestingWorkshop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiTestingWorkshop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
